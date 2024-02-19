3f9a38e19ccac000566c08887e74992a0ca92dff
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-02-19 15:25:45 +0600

    some js function added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-// alert()
+const allSeat = document.getElementsByClassName('seat');
+let available = document.getElementById('availabe');
+let count = 0;
+for (const seat of allSeat) {
+    seat.addEventListener('click', function (e) {
+        count = count + 1;
+        const availableSeat = parseInt(available.innerText) - count;
+        console.log(availableSeat);
+        const selectedSeat = parseInt(selected.innerText) + count;
+        console.log(selectedSeat);
+        available.innerText = availableSeat;
+        selected.innerText = selectedSeat;
+    })
+    
+}
