13c6b5a366366b747a199041f0cb8c4dccd5cf4b
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-02-19 19:11:58 +0600

    more js function added

--- scripts.js
+++ scripts.js
@@ -1,16 +1,44 @@
 const allSeat = document.getElementsByClassName('seat');
 for (const seat of allSeat) {
     seat.addEventListener('click', function () {
+
+
         let count = 0;
         count = count + 1;
-        let available = document.getElementById('availabe');
-        let selected = document.getElementById('selected');
+
+        const available = document.getElementById('availabe');
+        const selected = document.getElementById('selected');
+        const seat = document.getElementById('seat');
+        const seatName = 'A-Z||1-4'
+        const seatClass = 'Econonmy Class'
+        const seatPrice = 550;
+        const seatDeatails = document.getElementById('details');
+
         const availableSeat = parseInt(available.innerText) - count;
-        console.log
-            ('availableSeat', availableSeat);
         const selectedSeat = parseInt(selected.innerText) + count;
-        console.log('selectedSeat', selectedSeat);
+        const finalSeat = parseInt(seat.innerText) + count;
+
         available.innerText = availableSeat;
         selected.innerText = selectedSeat;
+        seat.innerText = finalSeat;
+
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.innerText = seatName;
+        const p2 = document.createElement('p');
+        p2.innerText = seatClass;
+        const p3 = document.createElement('p');
+        p3.innerText = seatPrice;
+
+        li.appendChild(p);
+        li.appendChild(p2);
+        li.appendChild(p3);
+        seatDeatails.appendChild(li);
+
+        const total = document.getElementById('total');
+        const totalPrice = parseInt(seat.innerText) * seatPrice;
+        total.innerText = totalPrice;
+
+        
     })
 }
