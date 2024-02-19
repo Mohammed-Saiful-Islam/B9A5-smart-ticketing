13487cd55e94566ca236566d9e7072deb5485bb7
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-02-19 20:13:06 +0600

    more js function added

--- scripts.js
+++ scripts.js
@@ -39,6 +39,27 @@ for (const seat of allSeat) {
         const totalPrice = parseInt(seat.innerText) * seatPrice;
         total.innerText = totalPrice;
 
-        
+        //  NEW15 Couple 20
+
+        const enterCoupon = document.getElementById('apply-input');
+        const grand = document.getElementById('grand');
+        const apply = document.getElementById('apply-btn')
+
+        apply.addEventListener('click', function () {
+            const offerPass = enterCoupon.value;
+            if (offerPass === 'NEW15') {
+                const discount15 = totalPrice * 0.15;
+                const grandTotal = totalPrice - discount15;
+                grand.innerText = grandTotal;
+
+            } else if (offerPass === 'Couple 20') {
+                const discount20 = totalPrice * 0.2;
+                const grandTotal = totalPrice - discount20;
+                grand.innerText = grandTotal
+            } else {
+                alert('please enter a valid coupon code');
+            }
+        })
+
     })
 }
